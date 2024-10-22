397046b5ae40a3f1195f13b9ea11c5ac1fa1e0dd
Thomas Lossio <thomaslossio@hotmail.com>
2024-10-22 18:55:08 -0300

    Performing queries with knex

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,19 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  // const transactions = await knex('transactions').insert({
+  //   id: crypto.randomUUID(),
+  //   title: 'Transação de teste',
+  //   amount: 1000,
+  // }).returning('*')
 
-  return tables
+  // const transactions = await knex('transactions').select('*')
+
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app
