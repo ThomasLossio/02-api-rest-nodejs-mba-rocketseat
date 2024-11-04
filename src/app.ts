84bfb19fa70c85632e6650b71bdf771f12c6c9b0
Thomas Lossio <thomaslossio@hotmail.com>
2024-11-04 19:26:43 -0300

    Remove hook from app

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,6 @@ export const app = fastify()
 
 app.register(cookie)
 
-app.addHook('preHandler', async (req) => {
-  console.log(`[${req.method}] ${req.url}`)
-})
-
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
